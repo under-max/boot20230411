8a08af661183b3d90bf499cb87e63853f90be1e5
under-max <saigrams@naver.com>
2023-04-11 15:14:00 +0900

    first

---
+++ src/main/java/com/example/demo/Boot20230411Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot20230411Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot20230411Application.class, args);
+	}
+
+}
